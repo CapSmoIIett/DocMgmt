1e00d834ddb49bac753d46f9963dbc005f852089
unknown <vikdrovosek@gmail.com>
2023-04-17 13:09:59 +0300

    Replace db to server.

--- Server/database.cpp
+++ Server/database.cpp
@@ -4,6 +4,20 @@
 
 #include <qrandom.h>
 
+/*
+        QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL", "mydb");
+       db.setHostName("DESKTOP-49Q6B8N");
+       db.setDatabaseName("DocMgmt");
+       db.setUserName("postgres");
+       db.setPassword("1111");
+       bool ok = db.open();
+       std::cout << "BD connect: " << ok << "\n";
+
+       QSqlQuery query;
+       query.exec("INSERT INTO EMPLOYEE (Rights_id, Full_Name, Birth_date, Office_id, phone_number) VALUES (0, 'name2', TO_DATE('1/1/2000', 'DD/MM/YYYY'), 0, '11111111')");
+
+ */
+
 DataBase::DataBase(QObject *parent) : QObject(parent)
 {
     // Подключаемся к базе данных
