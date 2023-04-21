d6c1439d4d1a1ce7144685c8d44c525dd6919e06
unknown <vikdrovosek@gmail.com>
2023-04-21 23:09:00 +0300

    Edit connect to host.

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,23 +1,72 @@
 #include "client.h"
 
 
-Client::Client()
+Client::Client(QString ip, int port) :
+    s_IP(ip),
+    i_Port(port)
+{
+    p_TcpSocket = new QTcpSocket(this);
+
+    connect(p_TcpSocket, &QTcpSocket::readyRead, this, &Client::ReadyRead);
+
+}
+
+void Client::ConnectToServer()
+{
+    qDebug() << "Connecting" << s_IP << " " << i_Port;
+    p_TcpSocket->connectToHost(s_IP, i_Port);
+
+    p_TcpSocket->waitForConnected();
+
+    qDebug() << "Connection: " << (p_TcpSocket->state() == QTcpSocket::ConnectedState);
+}
+
+bool Client::IsConnected()
+{
+    return p_TcpSocket->state() == QTcpSocket::ConnectedState;
+}
+
+void Client::SendToServer(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_4);
+    out << str;
+    p_TcpSocket->write(Data);
+}
+
+QString ReadFromServer()
 {
 
-    static int i = 0;
+}
 
-    if (i++ == 0)
+bool Client::Verify (QString username, QString password)
+{
+
+    if (!IsConnected())
+        ConnectToServer();
+
+    SendToServer(MSG_VERIFY_TEXT);
+    SendToServer(username);
+    SendToServer(password);
+
+}
+
+void Client::ReadyRead ()
+{
+    p_TcpSocket = (QTcpSocket*)sender();
+    QDataStream in(p_TcpSocket);
+
+    in.setVersion(QDataStream::Qt_6_4);
+    if(in.status() != QDataStream::Ok)
     {
-        QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL", "mydb");
-       db.setHostName("DESKTOP-49Q6B8N");
-       db.setDatabaseName("DocMgmt");
-       db.setUserName("postgres");
-       db.setPassword("1111");
-       bool ok = db.open();
-       std::cout << "BD connect: " << ok << "\n";
-
-       QSqlQuery query;
-       query.exec("INSERT INTO EMPLOYEE (Rights_id, Full_Name, Birth_date, Office_id, phone_number) VALUES (0, 'name2', TO_DATE('1/1/2000', 'DD/MM/YYYY'), 0, '11111111')");
-
+        std::cout << "Error" << "\n";
     }
+
+    std::cout << "Start reading ..." << "\n";
+    QString str;
+    in >> str;
+    //std::cout << str << "\n";
+    std::cout << "End reading." << "\n";
+
 }
--- Client/client.h
+++ Client/client.h
@@ -5,9 +5,31 @@
 #include <QtSql/QSqlDatabase>
 #include <QSqlQuery>
 
-class Client
+#include <QTcpServer>
+#include <QTcpSocket>
+
+#include "../messages.h"
+
+class Client : public QObject
 {
 public:
-    Client();
+    Client(QString ip = "127.0.0.1", int port = 5500);
+
+    void ConnectToServer();
+    bool IsConnected();
+    void SendToServer(QString str);
+    QString ReadFromServer();
+
+    bool Verify (QString username, QString password);
+
+public slots:
+    void ReadyRead();
+
+private:
+    QTcpSocket* p_TcpSocket;
+    QByteArray Data;
+
+    QString s_IP;
+    int i_Port;
 };
 
